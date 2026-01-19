8f9d574570e3ac154fb82baf1594e96987267f31
isakbh <isak@brunhenriksen.net>
2026-01-19 21:47:34 +0100

    next og previous knappene funker faktisk nå

--- actions/controls.php
+++ actions/controls.php
@@ -6,6 +6,16 @@ if($_GET['action'] === 'pause'){
     $mphpd->player()->pause();
     $playing_status = $mphpd->status(["state"]);
     echo "Pause er toggled. Status er nå: $playing_status";
+};
+
+if($_GET['action'] === 'previous'){
+    $mphpd->player()->previous();
+    echo "Gått til forrige sang.";
+};
+
+if($_GET['action'] === 'next'){
+    $mphpd->player()->next();
+    echo "Gått til neste sang.";
 }
 
 ?>
--- scripts/sigma.js
+++ scripts/sigma.js
@@ -5,10 +5,26 @@
 // |_|  |_|_|   |____/  |_|  |_|  \___/|_| |_|\__\___|_| |_|\__,_|
 
 const pausebutton = document.getElementById('pause-button');
+const previousbutton = document.getElementById('previous-button');
+const nextbutton = document.getElementById('next-button');
 
 pausebutton.addEventListener('click', () => {
-    console.log("Bro trykket på pause lol")
+    console.log("Bro trykket på pause lol");
     fetch('actions/controls.php?action=pause')
+        .then(response => response.text())
+        .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
+})
+
+previousbutton.addEventListener('click', () => {
+    console.log("Bro vil høre på forrige sang");
+    fetch('actions/controls.php?action=previous')
+        .then(response => response.text())
+        .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
+})
+
+nextbutton.addEventListener('click', () => {
+    console.log("Bro vil høre på neste sang :rofl::rofl::rofl:");
+    fetch('actions/controls.php?action=next')
         .then(response => response.text())
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
 })
