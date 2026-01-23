2fcd6706d42b263483b274b49413ac3428ab8f3b
IsakBH <isak@brunhenriksen.net>
2026-01-23 22:31:28 +0100

    enkel styling

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $status = $mphpd->status();
     <link rel="icon" href="../Pictures/Tux.svg.png">
     <script src="../Javascript/toggle.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/player.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" />
     <meta charset="utf-8">
 
