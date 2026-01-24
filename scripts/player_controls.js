fcac9f61ec577b34ebb6dd4a9952e0c4b195d6dd
isakbh <isak@brunhenriksen.net>
2026-01-24 12:53:18 +0100

    the user can now click on the album art to pause the song

--- scripts/player_controls.js
+++ scripts/player_controls.js
@@ -11,7 +11,9 @@ const previousbutton = document.getElementById('previous-button');
 const nextbutton = document.getElementById('next-button');
 const skipbackwardsbutton = document.getElementById('skip-backwards-button');
 const skipforwardsbutton = document.getElementById('skip-forwards-button');
+const playeralbumart = document.getElementById('playeralbumart');
 
+// pausing
 pausebutton.addEventListener('click', () => {
     console.log("Bro trykket på pause lol");
     fetch('actions/controls.php?action=pause')
@@ -19,6 +21,14 @@ pausebutton.addEventListener('click', () => {
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
 });
 
+playeralbumart.addEventListener('click', () => {
+    console.log("Bro trykket på pause lol");
+    fetch('actions/controls.php?action=pause')
+        .then(response => response.text())
+        .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
+});
+
+// going to the previous song in the queue
 previousbutton.addEventListener('click', () => {
     console.log("Bro vil høre på forrige sang");
     fetch('actions/controls.php?action=previous')
@@ -26,6 +36,7 @@ previousbutton.addEventListener('click', () => {
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
 });
 
+// going to the next song in the queue
 nextbutton.addEventListener('click', () => {
     console.log("Bro vil høre på neste sang :rofl::rofl::rofl:");
     fetch('actions/controls.php?action=next')
@@ -33,6 +44,7 @@ nextbutton.addEventListener('click', () => {
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
 });
 
+// skipping/seeking backwards in the current song
 skipbackwardsbutton.addEventListener('click', () => {
     console.log("Bro vil gå bakover?? :DROOFL::DROOFL:");
     fetch('actions/controls.php?action=skipback')
@@ -40,6 +52,7 @@ skipbackwardsbutton.addEventListener('click', () => {
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
 });
 
+// skipping/seeking forwards in the current song
 skipforwardsbutton.addEventListener('click', () => {
     console.log("Bro vil gå framover?? :DROOFL::DROOFL:");
     fetch('actions/controls.php?action=skipforwards')
