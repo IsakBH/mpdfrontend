6cea35354dae090a80a58b92e4574de3243a5a16
isakbh <isak@brunhenriksen.net>
2026-01-25 15:10:30 +0100

    the progress bar now feels much more responsive thanks to two of the most elegant and sexy functions i've ever written in progress_update.js

--- scripts/progress_update.js
+++ scripts/progress_update.js
@@ -1,22 +1,24 @@
 const progress_bar = document.getElementById('progress-bar');
 const progress_container = document.querySelector('.progress-container');
 
-async function update_progress_bar() {
-    const response = await fetch('actions/song_status.php');
-    const data = await response.json();
-    console.log(data['elapsed'])
-
-    progress_bar.style.width = data.percent + '%';
+async function update_progress_bar(percentage) {
+    if (percentage) {
+        progress_bar.style.width = percentage + '%';
+    } else {
+        const response = await fetch('actions/song_status.php');
+        const data = await response.json();
+        console.log(data['elapsed'])
+    }
 }
 
 window.onload = update_progress_bar();
 setInterval(update_progress_bar, 1000);
 
 async function skip_to_percentage(percentage) {
+    update_progress_bar(percentage);
     fetch('actions/controls.php?action=seektopercent&percent=' + percentage)
         .then(response => response.text())
         .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
-    update_progress_bar();
 };
 
 progress_container.addEventListener('click', function (e) {
