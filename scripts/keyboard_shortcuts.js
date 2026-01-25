bddd2f6addbac029c68ff84752b7757e1e8640f4
isakbh <isak@brunhenriksen.net>
2026-01-25 15:03:19 +0100

    added number keyboard shortcuts for skipping/seeking around in the track - 1 for 10%, 6 for 60%, etc

--- scripts/keyboard_shortcuts.js
+++ scripts/keyboard_shortcuts.js
@@ -1,3 +1,10 @@
+async function skip_to_percentage(percentage) {
+    fetch('actions/controls.php?action=seektopercent&percent=' + percentage)
+        .then(response => response.text())
+        .then(data => console.log("Heisann! Ts er det serveren sier: " + data));
+    update_progress_bar();
+};
+
 window.onkeydown = function (e) {
     // skip/seek backwards
     if (e.key === "ArrowLeft") {
@@ -27,5 +34,55 @@ window.onkeydown = function (e) {
     if (e.key === " ") {
         e.preventDefault();
         pausebutton.click();
+    }
+
+    if (e.key === "0") {
+        e.preventDefault();
+        skip_to_percentage(0);
+    }
+
+    if (e.key === "1") {
+        e.preventDefault();
+        skip_to_percentage(10);
+    }
+
+    if (e.key === "2") {
+        e.preventDefault();
+        skip_to_percentage(20);
+    }
+
+    if (e.key === "3") {
+        e.preventDefault();
+        skip_to_percentage(30);
+    }
+
+    if (e.key === "4") {
+        e.preventDefault();
+        skip_to_percentage(40);
+    }
+
+    if (e.key === "5") {
+        e.preventDefault();
+        skip_to_percentage(50);
+    }
+
+    if (e.key === "6") {
+        e.preventDefault();
+        skip_to_percentage(60);
+    }
+
+    if (e.key === "7") {
+        e.preventDefault();
+        skip_to_percentage(70);
+    }
+
+    if (e.key === "8") {
+        e.preventDefault();
+        skip_to_percentage(80);
+    }
+
+    if (e.key === "9") {
+        e.preventDefault();
+        skip_to_percentage(90);
     }
 }
