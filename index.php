0342b224fd8ce4bdc989ed7f701cd09d087be347
IsakBH <isak@brunhenriksen.net>
2026-01-26 23:35:11 +0100

    sorted the <head> in index.php

--- index.php
+++ index.php
@@ -8,13 +8,16 @@ $status = $mphpd->status();
 <html lang="en">
 
 <head>
+    <!-- Kjedelige ting -->
     <title> MPD Frontend </title>
     <link rel="icon" href="../Pictures/Tux.svg.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" href="styles/player.css">
     <link rel="stylesheet" href="styles/progress_bar.css">
     <link rel="stylesheet" href="styles/album_art.css">
-    <meta charset="utf-8">
 
     <!-- Fonter og ikoner -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
