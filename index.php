8e313b52327034a5dd1612e72354dc30475b1a91
isakbh <isak@brunhenriksen.net>
2026-01-31 21:28:43 +0100

    improved styling of the songs in the #queue-selector-container by making the colour of the artist display grey, in likeness to the #main-container, and made the #queue-song-play-button look way better

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ $status = $mphpd->status();
                 <span class='queue-song-artist'>{$artist}</span>
             </div>
             <div class=\"song-actions\">
-                <button class=\"play-button\" onclick=\"play_song({$id})\" title=\"Play song\">
+                <button class=\"queue-song-play-button\" onclick=\"play_song({$id})\" title=\"Play song\">
                     <i class=\"fa-solid fa-play\"></i>
                 </button>
             </div> </li>";
