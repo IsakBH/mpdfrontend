6d85942a540ba16fd3416f0c70b5b29af46964cf
isakbh <isak@brunhenriksen.net>
2026-02-04 23:09:05 +0100

    began on actual transparency setting functionality

--- scripts/apply_settings.js
+++ scripts/apply_settings.js
@@ -1,3 +1,18 @@
+function apply_transparency() {
+    const istransparent = localStorage.getItem('transparent') === 'true';
+    if (istransparent) {
+        document.body.classList.add('transparent');
+        console.log("transparent mode is enabled");
+    }
+    const toggleButton = document.getElementById('transparency-toggle');
+    if (toggleButton) {
+        toggleButton.checked = istransparent;
+    }
+}
+
 function toggle_transparency() {
-    settings_dialog.style.background = 'rgba(0, 0, 0, 0.80)';
+    const toggleButton = document.getElementById('transparency-toggle');
+    document.body.classList.toggle('transparent');
+    localStorage.setItem('transparent', toggleButton.checked);
+    apply_transparency();
 }
